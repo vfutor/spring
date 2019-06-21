a757c9eb2f473df5898ad3ab66789406dc7d5e69
vfutoryanskyy <vfutorianskyy@twilio.com>
2019-06-21 15:05:54 +0300

    Added some func

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,12 +1,10 @@
-package application;
+package com;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
